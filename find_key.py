2f8c2f77ac1dccb0c5fe5410dcb246cdc5ace23b
kondo takeshi <take.she12@gmail.com>
2016-12-25 16:58:03 +0900

    create method

--- find_key.py
+++ find_key.py
@@ -16,15 +16,33 @@ As = ['A#','C','D','D#','F','G','A']
 B = ['B','C#','D#','E','F#','G#','A#']
 keys = [C,Cs,D,Ds,E,F,Fs,G,Gs,A,As,B]
 
-# get args
-args = sys.argv
-args.pop(0)
-find_keys = []
+def main():
+  args = sys.argv
+  if len(args) == 1:
+    usage()
+  args.pop(0)
+  find_keys = find_key(args)
+  print_result(find_keys)
 
-for key in keys:
-  if set(key) > set(args):
-    print key
-    find_keys.append(key)
+def usage():
+  print "USAGE:python find_key.py <sound1> <sound2> ... <soundn>"
+  sys.exit()
 
-if len(find_keys) == 0:
-  print "not found key"
+def find_key(args):
+  find_keys = []
+  for key in keys:
+    if set(key) > set(args):
+      find_keys.append(key)
+  return find_keys
+
+def print_result(find_keys): 
+  if len(find_keys) == 0:
+    print "not found key"
+  else:
+    print "candidate(first sound is KEY):"
+    for key in find_keys:
+      print key
+
+if __name__ == '__main__':
+  main()
+
